cee2572719c8f99ec53fb7af077a8922866f44b7
gurjapsam <gurjapsamra01@gmail.com>
2017-06-15 19:22:01 -0700

    Uploading Lesson 2

--- Fundamentals II - Lesson 2 Object/src/CommissionEmployee3.java
+++ Fundamentals II - Lesson 2 Object/src/CommissionEmployee3.java
@@ -1,7 +1,7 @@
-// Fig. 9.4: CommissionEmployee.java
-// CommissionEmployee class represents a commission employee.
+// Fig. 9.12: CommissionEmployee3.java
+// CommissionEmployee3 class represents a commission employee.
 
-public class CommissionEmployee extends Object
+public class CommissionEmployee3
 {
    private String firstName;
    private String lastName;
@@ -10,7 +10,7 @@ public class CommissionEmployee extends Object
    private double commissionRate; // commission percentage
 
    // five-argument constructor
-   public CommissionEmployee( String first, String last, String ssn, 
+   public CommissionEmployee3( String first, String last, String ssn, 
       double sales, double rate )
    {
       // implicit call to Object constructor occurs here
@@ -19,7 +19,7 @@ public class CommissionEmployee extends Object
       socialSecurityNumber = ssn;
       setGrossSales( sales ); // validate and store gross sales
       setCommissionRate( rate ); // validate and store commission rate
-   } // end five-argument CommissionEmployee constructor
+   } // end five-argument CommissionEmployee3 constructor
 
    // set first name
    public void setFirstName( String first )
@@ -57,43 +57,44 @@ public class CommissionEmployee extends Object
       return socialSecurityNumber;
    } // end method getSocialSecurityNumber
 
-   // set commission employee's gross sales amount
+   // set gross sales amount
    public void setGrossSales( double sales )
    {
       grossSales = ( sales < 0.0 ) ? 0.0 : sales;
    } // end method setGrossSales
 
-   // return commission employee's gross sales amount
+   // return gross sales amount
    public double getGrossSales()
    {
       return grossSales;
    } // end method getGrossSales
-
-   // set commission employee's rate
+   
+   // set commission rate
    public void setCommissionRate( double rate )
    {
       commissionRate = ( rate > 0.0 && rate < 1.0 ) ? rate : 0.0;
    } // end method setCommissionRate
 
-   // return commission employee's rate
+   // return commission rate
    public double getCommissionRate()
    {
       return commissionRate;
    } // end method getCommissionRate
 
-   // calculate commission employee's pay
-   public double earnings()
+   // calculate earnings
+   public double getPaymentAmount()
    {
-      return commissionRate * grossSales;
+      return getCommissionRate() * getGrossSales();
    } // end method earnings
 
-   // return String representation of CommissionEmployee object
+   // return String representation of CommissionEmployee3 object
    public String toString()
    {
       return String.format( "%s: %s %s\n%s: %s\n%s: %.2f\n%s: %.2f", 
-         "commission employee", firstName, lastName, 
-         "social security number", socialSecurityNumber, 
-         "gross sales", grossSales, 
-         "commission rate", commissionRate );
+         "commission employee", getFirstName(), getLastName(), 
+         "social security number", getSocialSecurityNumber(), 
+         "gross sales", getGrossSales(), 
+         "commission rate", getCommissionRate() );
    } // end method toString
-} // end class CommissionEmployee
+} // end class CommissionEmployee3
+
